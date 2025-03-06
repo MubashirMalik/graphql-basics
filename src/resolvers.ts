e9134fc9456afd4e0dbf293f47c3529fdd291861
Mubashir Ahmed Malik <imubasherahmad@gmail.com>
2025-03-06 07:21:26 +0500

    basics done..

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -12,5 +12,35 @@ export const resolvers = {
         authors: () => db.authors,
         author: (_, args) => db.authors.find(author => author.id === args.id
         )
+    },
+    Game: {
+        reviews: (parent) => db.reviews.filter(review => review.game_id === parent.id)
+    },
+    Review: {
+        game: (parent) => db.games.find(game => game.id === parent.game_id),
+        author: (parent) => db.authors.find(author => author.id === parent.author_id)
+    },
+    Author: {
+        reviews: (parent) => db.reviews.filter(review => review.author_id === parent.id)
+    },
+    Mutation: {
+        createGame(_, args) {
+            const id = `${db.games.length + 1}`
+            db.games.push({ id, ...args.game })
+            return db.games.at(-1)
+        },
+        deleteGame(_, args) {
+            const game = db.games.find(game => game.id === args.id)
+            db.games = db.games.filter(game => game.id !== args.id)
+            return game
+        },
+        updateGame(_, args) {
+            for (let i = 0; i < db.games.length; i++) {
+                if (db.games[i].id === args.id) {
+                    db.games[i] = {...db.games[i], ...args.game } 
+                    return db.games[i]
+                }
+            }
+        }
     }
 }
--- src/schema.ts
+++ src/schema.ts
@@ -3,22 +3,26 @@ export const typeDefs = `#graphql
     type Game {
         id: ID!
         title: String!
-        platform: [String]!
+        platform: [String!]!
+        reviews: [Review!]
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
    
-   type Review {
+    type Review {
         id: ID!
         rating: Int!
         content: String!
-   }
+        author: Author!
+        game: Game!
+    }
 
-   type Query {
+    type Query {
         games: [Game]
         game(id: ID!): Game
 
@@ -27,5 +31,21 @@ export const typeDefs = `#graphql
 
         reviews: [Review]
         review(id: ID!): Review 
+   }
+
+    type Mutation {
+        createGame(game: CreateGameInput!): Game
+        deleteGame(id: ID!): Game
+        updateGame(id: ID!, game: UpdateGameInput!): Game
+    }
+
+    input CreateGameInput {
+        title: String!, 
+        platform: [String!]!
+   }
+
+   input UpdateGameInput {
+        title: String,
+        platform: [String!]
    }
 `
